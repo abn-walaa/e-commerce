8d8805cbf3f0fc35d670047cc5e61455de10afe0
Mohammed Walaa <mohammedwalaar.sw@student.uobabylon.edu.iq>
2023-06-24 15:08:11 +0300

    :D

--- src/app.js
+++ src/app.js
@@ -8,8 +8,14 @@ const app = express()
 app.use(express.json())
 // log router 
 const log = require('./routers/log')
-
 app.use('/log', log);
+//  products 
+const product = require('./routers/products')
+app.use('/products', product)
+//  collections 
+const collections = require('./routers/collection')
+app.use('/products', collections)
+
 app.listen(3000, () => {
     console.log("server is up")
 })
